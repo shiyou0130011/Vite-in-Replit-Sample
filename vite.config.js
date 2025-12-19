258da1e280082ac3676302a5f3f86a05a943aab6
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2025-12-19 13:26:07 +0800

    Modify: 排版

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@
 import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
-  }
+	const env = loadEnv(mode, process.cwd(), '')
+	return {
+		define: {
+			__APP_ENV__: JSON.stringify(env.APP_ENV),
+		},
+	}
 })
